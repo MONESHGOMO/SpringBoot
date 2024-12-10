56af0cb289db1f85dda1d0035c75ab2624c8d2b7
GOMO_PROGRAMMER <167512723+MONESHGOMO@users.noreply.github.com>
2024-12-10 22:58:13 +0530

    Delete BackendProject directory

--- BackendProject/Gomo/src/main/java/com/GomoCodes/Gomo/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.GomoCodes.Gomo.repository;
-
-import com.GomoCodes.Gomo.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
